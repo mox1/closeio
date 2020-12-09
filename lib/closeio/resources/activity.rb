9184c83f8a234cd870f4434f715ba06de9237cdd
dmitrytrager <dmitry.trager@revealbot.com>
2020-12-09 14:40:25 +0300

    Add list_meetings for activity resource

--- lib/closeio/resources/activity.rb
+++ lib/closeio/resources/activity.rb
@@ -109,6 +109,14 @@ module Closeio
         delete("#{sms_path}#{id}/")
       end
 
+      #
+      #  Meeting Activities
+      #
+
+      def list_meetings(options = {})
+        get(meeting_path, options)
+      end
+
       private
 
       def activity_path
@@ -133,6 +141,10 @@ module Closeio
 
       def sms_path
         "#{activity_path}sms/"
+      end
+
+      def meeting_path
+        "#{activity_path}meeting/"
       end
     end
   end
